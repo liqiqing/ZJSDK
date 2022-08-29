a3f21edb35c5ab37f2781d7bcb4bc0315e5bc579
Mountain King <1054293440@qq.com>
2022-08-29 17:33:02 +0800

    删除podfile

--- ZJSDKDemo/Pods/Headers/Private/GDTMobSDK/GDTSplashAdNetworkAdapterProtocol.h
+++
@@ -1,1 +0,0 @@
-../../../GDTMobSDK/lib/GDTSplashAdNetworkAdapterProtocol.h
--- ZJSDKDemo/Pods/Target Support Files/Pods-ZJSDKDemo/Pods-ZJSDKDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZJSDKDemo : NSObject
-@end
-@implementation PodsDummy_Pods_ZJSDKDemo
-@end
